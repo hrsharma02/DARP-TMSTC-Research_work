be522cdd327d891641dbe07e42ecbc870e4f270c
Harish Sharma <82651854+hrsharma02@users.noreply.github.com>
2026-04-05 11:33:35 +0530

    init file

---
+++ DARPTMSTC/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+"""
+DARP (Divide Areas Algorithm for Optimal Multi-Robot Coverage)
+Module for workload-balanced region partitioning.
+"""
+
+from .darp import DARP
+
+__all__ = ['DARP']
